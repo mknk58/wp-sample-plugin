434ea8d1201e3dbf26b0144129463b6578a2bf2f
mknk58 <mk-5858-bf@ezweb.ne.jp>
2018-10-18 21:08:06 +0900

    プラグインのカプセル化について。

--- wp-sample-plugin.php
+++ wp-sample-plugin.php
@@ -8,3 +8,55 @@ Author: Miku Imai
 Author URI: https://github.com/mknk58/wp-sample-plugin
 License: GPLv2 or leter
 */
+class Human {
+	public $para = array();
+
+	public function __construct( $para ){
+		$this->para = $para;
+
+	}
+	public function cry() {
+		echo $this->para['voice'];
+	}
+
+	public function attack() {
+		$range = rand(0,5);
+		$sign  = rand(0,1);
+		if (sign === 0){
+			$range = $range * -1;
+		}
+		$range += $this->para['power'];
+		echo $this->para['name'] . 'の攻撃！<br>';
+		echo $this->para['name'] . 'は、' . $range . 'のダメージを与えた！';
+
+		return $range;
+	}
+
+	public function die_flag( $damage, $name ){
+		$this->para['hp'] -= $damage;
+		if( $this->para['hp'] <= 0 ){
+			echo $this->para['name'] . 'は、' . $name . 'に殴られた！';
+			echo $this->para['name'] . 'は、死んでしまった.....';
+		}
+	}
+}
+$hazama_para = array(
+	'name'  => 'Yoshiki',
+	'hp'    => 10,
+	'power' => 10,
+	'speed' => 500,
+	'voice' => 'おぎゃああああ！！'
+);
+
+$imai_para = array(
+	'name'  => 'Miku',
+	'hp'    => 100,
+	'power' => 30,
+	'speed' => 5,
+	'voice' => 'ひゃああああああ！！'
+);
+$hazama = new Human( $hazama_para );
+$imai = new Human( $imai_para );
+
+$damage = $imai->attack();
+$hazama->die_flag( $damage, $imai->para['name'] );
