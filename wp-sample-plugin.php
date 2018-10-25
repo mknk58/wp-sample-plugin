990c4e4eabfc82341d549f81eae0556075b7f656
mknk58 <mk-5858-bf@ezweb.ne.jp>
2018-10-25 20:56:50 +0900

    プラグイン三回目

--- wp-sample-plugin.php
+++ wp-sample-plugin.php
@@ -37,5 +37,50 @@ class Sample_Plugin {
 			array( $this, 'list_page_render' ),
 			'dashicons-admin-site'
 		);
+		add_submenu_page(
+			__FILE__,
+			'サンプル一覧',
+			'サンプル一覧',
+			'manage_options',
+			plugin_basename( __FILE__ ),
+			array( $this, 'list_page_render' ),
+			'dashicons-admin-site'
+		);
+		add_submenu_page(
+			__FILE__,
+			'サンプル登録',
+			'サンプル登録',
+			'manage_options',
+			plugin_dir_path( __FILE__ ) . 'includes/wp-sample-plugin-post.php',
+			array( $this, 'post_page_render' ),
+			'dashicons-admin-site'
+		);
+	}
+	/**
+	* Rendaring List Page.
+	*
+	* @version 1.0.0
+	* @since   1.0.0
+	*/
+	public function list_page_render(){
+		require_once( plugin_dir_path( __FILE__ ) . 'includes/wp-sample-plugin-list.php' );
+		new Sample_Plugin_List();
+	}
+	
+	/**
+	* Rendaring Post Page.
+	*
+	* @version 1.0.0
+	* @since   1.0.0
+	*/
+	public function post_page_render(){
+		require_once( plugin_dir_path( __FILE__ ) . 'includes/wp-sample-plugin-post.php' );
+		new Sample_Plugin_Post();
 	}
 }
+
+
+
+
+
+
