cfb4c57ac0ddf969aed300f9d3a49e76b7a2ff8d
mknk58 <mk-5858-bf@ezweb.ne.jp>
2018-12-20 21:01:46 +0900

    20181220のコミット。削除を表示しました。

--- includes/wp-sample-admin-db.php
+++ includes/wp-sample-admin-db.php
@@ -182,30 +182,9 @@ class Sample_Plugin_Admin_Db{
 	public function delete_options( $id ){
 		global $wpdb;
 			
-
-			
-			$key = array( 'id' -> $id );
-			$key_prepared = array( '%d' );
-			
-			$wpdb->update( $this->table_name, $key, $key_prepared );
-		}
+		$key = array( 'id' => $id );
+		$key_prepared = array( '%d' );
+		
+		$wpdb->delete( $this->table_name, $key, $key_prepared );
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
-
+}
--- includes/wp-sample-plugin-list.php
+++ includes/wp-sample-plugin-list.php
@@ -15,6 +15,13 @@ class Sample_Plugin_List{
 	*/
 	public function __construct() {
 		$db = new Sample_Plugin_Admin_Db();
+		
+		if ( isset( $_GET[ 'mode' ] ) && $_GET[ 'mode' ] === 'delete' ) {
+			if ( isset( $_GET[ 'id' ] ) && is_numeric($_GET[ 'id' ] ) ) {
+				$db->delete_options( $_GET[ 'id' ] );
+				$this->information_render();
+			}
+		}
 		$this->page_render( $db );
 	}
 	
@@ -26,6 +33,7 @@ class Sample_Plugin_List{
 	*/
 	private function page_render( $db ){
 		$post_url = admin_url() . 'admin.php?page=wp-sample-plugin/includes/wp-sample-plugin-post.php';
+		$self_url = $_SERVER[ 'PHP_SELF' ] . '?' . $_SERVER[ 'QUERY_STRING' ];
 		
 		$html  = '<div class="wrap">';
 		$html .= '<h1 class="wp-heading-inline">サンプル一覧</h1>';
@@ -49,6 +57,7 @@ class Sample_Plugin_List{
 				$html .= '<td>' . $row->how_display . '</td>';
 				$html .= '<td>' . $row->filter_category . '</td>';
 				$html .= '<td><a href="' . $post_url . '&id=' . $row->id . '">編集</a></td>';
+				$html .= '<td><a href="' . $self_url . '&mode=delete&id=' . $row->id . '">削除</a></td>';
 				$html .= '</tr>';
 			}
 		} else{
@@ -61,6 +70,22 @@ class Sample_Plugin_List{
 		$html .= '</div>';
 		echo $html;
 	}
+	
+	/**
+	* Information Message Render
+	*
+	* @version 1.0.0
+	* @since 1.0.0
+	*/
+	private function information_render(){
+		$html  = '<div id="message" class="updated notice-success is-dismissible blow-h2">';
+		$html .= '<p>削除されました。(^^)</p>';
+		$html .= '<button type="button" class="notice-dismiss"></button>';
+		$html .= '</div>';
+		
+		echo $html;
+	}
+	
 }
 
 
